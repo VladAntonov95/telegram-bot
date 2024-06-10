8d1a213ee8351ff98fad1dfafd7651e83bff40d7
VladAntonov95 <vladislav.95antonov@gmail.com>
2024-06-10 15:07:10 +0300

    add toggle button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Button from "../button/Button";
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
-  const tg = window.Telegram.WebApp;
-
-  const onClose = () => {
-    tg.close();
-  };
+  const { tg, user, onClose } = useTelegram();
 
   return (
     <div className={"header"}>
       <Button onClick={onClose}>Закрыть</Button>
-      <span className={"username "}>{tg.initDataUnsafe?.user?.username}</span>
+      <span className={"username "}>{user?.username}</span>
     </div>
   );
 };
